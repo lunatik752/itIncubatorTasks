2d475737df0d9972188a1fc40b2b64acaf022c49
lunatik752 <whitemaxim752@gmail.com>
2020-03-21 13:59:23 +0300

    added name output

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={s.header}>
+            <h1>
+                Белый Максим
+            </h1>
+        </div>
+    )
+};
+
+export default Header;
